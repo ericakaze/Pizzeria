b8f5a5397bc6f3c28cea1b7719fd080607901ac6
ericakaze <erica_kaze@baylor.edu>
2020-04-28 21:52:57 -0500

    added pages

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from . import views
 app_name='pizzas'
 urlpatterns=[path('', views.index, name='index'),
+#page that shows all Pizzas
+path('pizzas/', views.pizzas, name='pizzas'),
+path('pizzas/<int:topic_id>/',views.pizza, name='pizza'),
 ]
+
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import Pizza 
+
 def index(request):
     """ the home page for pizzeria"""
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    "show all pizzas"
+    pizzas= Pizza.objects.order_by('date_added')
+    context= {'pizzas':pizzas}
+    return render(request,'pizzas/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    "shows a single pizza and all its toppings"
+    pizza= Pizza.objects.get(id=topic_id)
+    toppings=pizza.entry_set.order_by('-date_added')
+    context= {'pizza': pizza, 'toppings':toppings}
+    return render (request, 'pizzas/pizza.html', context)
